f291fd04d8f5f6d43fb670c94d2326928fadaeb3
vrtx <s.vertegel@gmail.com>
2022-02-01 23:03:52 +0300

    add two numbers comparison

---
+++ two_num_comparison.rb
@@ -0,0 +1,16 @@
+puts "Type first number:"
+num1 = gets.chomp
+puts "Type second number:"
+num2 = gets.chomp
+
+def comparison(num1, num2)
+    if num1 == num2
+      puts "Numbers are equal!"
+    elsif num1 > num2
+      puts "First number is greater!"
+    else num1 < num2
+      puts "Second number is greater!"
+    end
+end
+
+puts comparison(num1, num2)
