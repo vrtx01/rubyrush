0c001cfdfcfef2a661e520fb5b10a8487b433799
vrtx <s.vertegel@gmail.com>
2022-02-02 22:14:43 +0300

    add odd or even number determiner

---
+++ odd_or_even.rb
@@ -0,0 +1,12 @@
+puts "Enter your value:"
+x = gets.chomp.to_i
+
+def odd_or_even(x)
+    if x % 2 == 0
+      puts "This number is even!"
+    else 
+      puts "This number is odd!"
+    end
+end
+
+puts "#{odd_or_even(x)}"
