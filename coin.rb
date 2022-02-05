0bc5dcdbb2fd58c8ee35028583f9890b76835d85
vrtx <s.vertegel@gmail.com>
2022-02-05 23:17:08 +0300

    add edge case and some minor changes

--- coin.rb
+++ coin.rb
@@ -1,13 +1,17 @@
-# Coin flip program
+# Coin flip game
 
-puts "Heads(0) or Tail(1)?"
+puts "Heads(Type: 0) or Tails(Type: 1)?"
 coin = gets.chomp.to_i
 
 flip = Random.new
 result = flip.rand(2)
+edge = Random.new
+edge = edge.rand(10)
 
 if coin == result
   puts "#{result}. You win!"
+elsif coin == edge
+  puts "Coin lands on its edge! OMG!"
 else
   puts "#{result}. You loose! Try again."
 end
