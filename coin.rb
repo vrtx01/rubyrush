13d64e6a5d97c30f67be0c197027c78c797063c1
vrtx <s.vertegel@gmail.com>
2022-02-06 23:25:34 +0300

    add error message for wrong entered arguments

--- coin.rb
+++ coin.rb
@@ -8,10 +8,14 @@ result = flip.rand(2)
 edge = Random.new
 edge = edge.rand(10)
 
+if coin < 0 || coin > 1
+  abort "Wrong number. Use '0' for Heads or '1' for Tails."
+end
+
 if coin == result
-  puts "#{result}. You win!"
+  puts "#{result} You win!"
 elsif coin == edge
   puts "Coin lands on its edge! OMG!"
 else
-  puts "#{result}. You loose! Try again."
+  puts "#{result} You loose! Try again."
 end
