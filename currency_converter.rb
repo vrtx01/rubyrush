90ec14ce1badd1fc8624e85c564994268be992f4
vrtx <s.vertegel@gmail.com>
2022-02-08 21:40:49 +0300

    add choice of currency in converter

--- currency_converter.rb
+++ currency_converter.rb
@@ -1,10 +1,14 @@
+puts "What currency do you have? (1) for Rubles, (2) for Dollars:"
+currency_choice = gets.chomp
 puts "What is the dollar buying rate today?:"
-dollar = gets.chomp.to_f
-puts "How much rubles do you have?:"
-ruble = gets.chomp.to_i
+dollar_rate = gets.chomp.to_f
 
-def currency(dollar, ruble)
-    return "After conversion you will have $#{(ruble / dollar).round(2)}"
+if currency_choice == '1'
+    puts "How much rubles do you have?:"
+    rubles_amount = gets.chomp.to_i
+    puts "After conversion you will have $#{(rubles_amount / dollar_rate).round(2)}"
+else currency_choice == '2'
+    puts "How much dollars do you have?:"
+    dollars_amount = gets.chomp.to_i
+    puts "After conversion you will have #{(dollar_rate * dollars_amount).round(2)} Rub"
 end
-
-puts currency(dollar, ruble)
