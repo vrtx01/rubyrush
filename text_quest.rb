2590c9065fdf9e46f82d2a336d320fd07abf0543
vrtx <s.vertegel@gmail.com>
2022-02-09 15:37:06 +0300

    fix some alignement mistakes

--- text_quest.rb
+++ text_quest.rb
@@ -14,15 +14,15 @@ else first_choice == '3'
 end
 
 puts "What are you going to do next? \n
-(1). Jump to the windowsill. Maybe you can see something outside.\n
-(2). Call the cops! I've been robbed! Where is my iPhone?\n
-(3). Jump down from the bed and try to look closer at that strange crystal on the floor."
+    (1). Jump to the windowsill. Maybe you can see something outside.\n
+    (2). Call the cops! I've been robbed! Where is my iPhone?\n
+    (3). Jump down from the bed and try to look closer at that strange crystal on the floor."
 second_choice = gets.chomp
 
 if second_choice == '1'
-        puts "You're now a fat cat! Do you forget that? That's your first jump as a cat and you flyed through the widow where you lost one of your 9 lives! What a waste! Your quest is over!"
-    elsif second_choice == '2'
-        puts "You're a cat! How are you going to use the phone? 'Hey, Siri! I'm a fat cat! Call the cops, meow!' Your quest is over!"
-    else second_choice == '3'
-        puts "You jumped down to that strange glowing crystal on the floor.\nGet as close as you can and looked inside of the pulsing light of the magic crystal.\nAnd inside that admirable glowing you can see a text that says 'Wake up you lazy!\nIt's time to make a text based quest in Ruby about magic transormation into a fat cat!'."
+    puts "You're now a fat cat! Do you forget that? That's your first jump as a cat and you flyed through the widow where you lost one of your 9 lives! What a waste! Your quest is over!"
+elsif second_choice == '2'
+    puts "You're a cat! How are you going to use the phone? 'Hey, Siri! I'm a fat cat! Call the cops, meow!' Your quest is over!"
+else second_choice == '3'
+    puts "You jumped down to that strange glowing crystal on the floor.\nGet as close as you can and looked inside of the pulsing light of the magic crystal.\nAnd inside that admirable glowing you can see a text that says 'Wake up you lazy!\nIt's time to make a text based quest in Ruby about magic transormation into a fat cat!'."
 end
