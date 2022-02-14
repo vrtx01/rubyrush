6542c0759c742bc7eb1c589a99f18a0a3b5a6a0e
Sergey Vertegel <s.vertgel@gmail.com>
2022-02-14 21:20:59 +0300

    add array of cars file

---
+++ array_of_cars.rb
@@ -0,0 +1,21 @@
+cars = [
+  "Audi",
+  "Bentley",
+  "BMW",
+  "Fiat",
+  "Skoda",
+  "Kia",
+  "Tesla",
+  "VW",
+  "Ferrari",
+  "Renault"
+]
+
+puts "We have #{cars.length} cars. Which will you choose?(1-10): "
+select = gets.chomp.to_i
+
+if select < 0 || select > 10
+  puts "Wrong number!"
+else
+  puts "Your car is: #{cars[select - 1]}! Congratulation!"
+end
