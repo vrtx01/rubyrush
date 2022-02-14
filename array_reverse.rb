650813e0f738f119de7869ac155b287941d64777
Sergey Vertegel <s.vertgel@gmail.com>
2022-02-14 21:10:14 +0300

    add array reverse file

---
+++ array_reverse.rb
@@ -0,0 +1,5 @@
+array = [1, 2, 3, 4, 5]
+puts "Initial Array: \n#{array}"
+puts "Reversed Array: \n#{array.reverse}"
+puts "Array doesn't changed: \n#{array}"
+puts "Changed Array: \n#{array.reverse!}"
