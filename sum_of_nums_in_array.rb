32488b3296405a1c1eaf6afeae58aee573ba43ca
vrtx <s.vertegel@gmail.com>
2022-02-18 22:09:46 +0300

    add sum of numbers in array calc

---
+++ sum_of_nums_in_array.rb
@@ -0,0 +1,18 @@
+puts "Enter last number in array:"
+num = gets.to_i
+def sum_of_nums_in_array(num)
+  arr = []
+  count = 1
+  while count <= num do
+    arr << count
+    count += 1
+  end
+
+print "Array: #{arr}\n"
+
+  sum = 0
+  arr.each { |i| sum += i }
+puts "Sum of numbers in array: #{sum}"
+end
+
+sum_of_nums_in_array(num)
