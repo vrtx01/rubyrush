5ca2b7de86dea54b951d96959275e9f5e221577e
Sergey Vertegel <s.vertegel@gmail.com>
2022-03-24 23:31:51 +0300

    fix style to ruby way

--- coin.rb
+++ coin.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 # Coin flip game
 
-puts "Heads(Type: 0) or Tails(Type: 1)?"
+puts 'Heads(Type: 0) or Tails(Type: 1)?'
 coin = gets.chomp.to_i
 
 flip = Random.new
@@ -8,14 +10,12 @@ result = flip.rand(2)
 edge = Random.new
 edge = edge.rand(10)
 
-if coin < 0 || coin > 1
-  abort "Wrong number. Use '0' for Heads or '1' for Tails."
-end
+abort "Wrong number. Use '0' for Heads or '1' for Tails." if coin.negative? || coin > 1
 
 if coin == result
   puts "#{result} You win!"
 elsif coin == edge
-  puts "Coin lands on its edge! OMG!"
+  puts 'Coin lands on its edge! OMG!'
 else
   puts "#{result} You loose! Try again."
 end
