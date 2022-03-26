6909e182cac66443c0949db1dd8b4aa63710504a
Sergey Vertegel <s.vertegel@gmail.com>
2022-03-26 17:41:14 +0300

    fix rubocop errors

--- holidays_or_not.rb
+++ holidays_or_not.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 time = Time.now
 week_day = time.wday
 
 if week_day.between?(1, 5)
-  puts "Business day!"
-else 
-  puts "Holiday!"
+  puts 'Business day!'
+else
+  puts 'Holiday!'
 end
