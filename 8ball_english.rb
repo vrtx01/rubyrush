3e195292d4dbc58e7ee7ea1532cdfd0ca252d556
Sergey Vertegel <s.vertegel@gmail.com>
2022-03-26 17:24:52 +0300

    fix rubocop errors

--- 8ball_english.rb
+++ 8ball_english.rb
@@ -1,42 +1,44 @@
-puts "Enter your question human:"
-hello = gets.chomp
+# frozen_string_literal: true
+
+puts 'Enter your question human:'
+gets.chomp
 
 greeting = [
-  "Behold!:",
-  "Only who question get the answers:",
-  "Greetings mortal! Your answer is:"
+  'Behold!:',
+  'Only who question get the answers:',
+  'Greetings mortal! Your answer is:'
 ]
 
 puts greeting.sample
 
 answers = [
-# positive
-  "It is certain",
-  "It is decidedly so",
-  "Without a doubt",
-  "Yes definitely",
-  "You may rely on it",
+  # positive
+  'It is certain',
+  'It is decidedly so',
+  'Without a doubt',
+  'Yes definitely',
+  'You may rely on it',
 
-# affirmative
-  "As I see it, yes",
-  "Most likely",
-  "Outlook good",
-  "Yes",
-  "Signs point to yes",
+  # affirmative
+  'As I see it, yes',
+  'Most likely',
+  'Outlook good',
+  'Yes',
+  'Signs point to yes',
 
-# non-committal
-  "Reply hazy, try again",
-  "Ask again later",
-  "Better not tell you now",
-  "Cannot predict now",
-  "Concentrate and ask again",
+  # non-committal
+  'Reply hazy, try again',
+  'Ask again later',
+  'Better not tell you now',
+  'Cannot predict now',
+  'Concentrate and ask again',
 
-# negative
-  "Don't count on it",
-  "My reply is no",
-  "My sources say no",
-  "Outlook not so good",
-  "Very doubtful"
+  # negative
+  "don't count on it",
+  'My reply is no',
+  'My sources say no',
+  'Outlook not so good',
+  'Very doubtful'
 
 ]
 
