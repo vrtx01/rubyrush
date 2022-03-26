519e7ef8e1f2fb04ea43a2004724ac91ea8666b2
Sergey Vertegel <s.vertegel@gmail.com>
2022-03-27 01:41:08 +0300

    fix rubocop warnings

--- two_num_comparison.rb
+++ two_num_comparison.rb
@@ -1,15 +1,17 @@
-puts "Type first number:"
+# frozen_string_literal: true
+
+puts 'Type first number:'
 num1 = gets.chomp
-puts "Type second number:"
+puts 'Type second number:'
 num2 = gets.chomp
 
 def comparison(num1, num2)
   if num1 == num2
-    puts "Numbers are equal!"
+    puts 'Numbers are equal!'
   elsif num1 > num2
-    puts "First number is greater!"
-  else num1 < num2
-    puts "Second number is greater!"
+    puts 'First number is greater!'
+  elsif num1 < num2
+    puts 'Second number is greater!'
   end
 end
 
