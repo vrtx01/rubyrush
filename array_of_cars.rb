33b76f6b7e735aaefde2a1aff0cf14b1f74c6ac7
Sergey Vertegel <s.vertegel@gmail.com>
2022-03-27 02:42:55 +0300

    fix rubocop style warnings

--- array_of_cars.rb
+++ array_of_cars.rb
@@ -1,21 +1,23 @@
-cars = [
-  "Audi",
-  "Bentley",
-  "BMW",
-  "Fiat",
-  "Skoda",
-  "Kia",
-  "Tesla",
-  "VW",
-  "Ferrari",
-  "Renault"
+# frozen_string_literal: true
+
+cars = %w[
+  Audi
+  Bentley
+  BMW
+  Fiat
+  Skoda
+  Kia
+  Tesla
+  VW
+  Ferrari
+  Renault
 ]
 
 puts "We have #{cars.length} cars. Which will you choose?(1-10): "
 select = gets.chomp.to_i
 
-if select < 0 || select > 10
-  puts "Wrong number!"
+if select.negative? || select > 10
+  puts 'Wrong number!'
 else
   puts "Your car is: #{cars[select - 1]}! Congratulation!"
 end
